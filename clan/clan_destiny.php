1862103b95f52b53dc6a4a8530f859d4509822c3
ADMIN-PC\Admin <mikeymm64@gmail.com>
2017-06-19 19:47:24 +0200

    Version 1.0.2 Die Clan Destiny Seite habe ich ein Bissen weitergeschrieben. Es gibt gerade einen Nervigen bug, der das Badge hover nicht macht :( ich werde versuchen mit dem nächten patches es zu fixen. Sonst habe ich das Hintergrund und das Layout schon fertig.

--- clan/clan_destiny.php
+++ clan/clan_destiny.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <head>
 <!-- Favicon Pc -->
@@ -11,16 +11,20 @@
 	<!-- ################################################################################################################### -->
 	<!-- CSS & Fonts -->
 	<!-- ################################################################################################################### -->
-
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/badges.css">
+	<link rel="stylesheet" type="text/css" href="../css/popover.css">
 	<link rel="stylesheet" type="text/css" href="../css/clan/clan_d_layout.css">
 	<link href="http://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="../css/badges.css">
 	
 	<!-- ################################################################################################################### -->
 	<!-- JQuery -->
 	<!-- ################################################################################################################### -->
-	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+	</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="../js/jquery.js"></script>
 	<script src="../js/scroll.js"></script>
 	
@@ -53,3 +57,29 @@
 	</nav>
 
 </header>
+	<section id="Startseite" class="ka_page"
+	
+		<!-- Headline -->
+		<<h2><u>Destiny Clan</u></h2>
+		<div class="ka_h2_balken"></div>
+			<p class="Member">Dies sind Unsere Mitglieder:</p>
+		<	<div class="PlayStation">
+				<ul>PlayStation:
+				<ul>Grützi7<span class="pop player-badge player-badge--creator" title="Creator" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Er hat uns Ideen für unsere Seite dagelassen!">Creator</span></ul>
+				<ul>RRCRoadyJam_xd</ul>
+				<ul>xFLANK88PLAYERx</ul>
+				<ul>mkahli70</ul>
+				<ul>Robociller16</ul>
+				<ul>xX_THEBIEST_Xx</ul>
+				<ul>brunke01</ul>
+				<ul>gamer_3456_1</ul>
+				<ul>dagobert1411</ul>
+				<ul>nicomagpaluten</ul>
+				<ul>coolgamer44302</ul>
+				<ul>Chrasher15</ul>
+				<ul>MaJo_lp</ul>
+				<ul>Fungum88</ul>
+				<ul>consha___</ul>
+			</div>
+	</section>
+		<script type="text/javascript" src="../js/popover.js"></script>
